de8ff8155ae78787542158789345708141b0459d
dada992 <olive_js_933@naver.com>
2022-12-22 17:44:28 +0900

    [design] 레이아웃 수정

--- src/components/Layout/Sidebar/SidebarStyle.js
+++ src/components/Layout/Sidebar/SidebarStyle.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const SideBar = styled.div`
   width: 13.75rem;
@@ -12,48 +12,48 @@ export const SideBar = styled.div`
   overflow-x: hidden;
 `;
 
-/* .side-title {
+export const SideTitle = styled.div`
   font-size: 0.75rem;
   margin-bottom: 1.5rem;
   margin-top: 100px;
-}
+`;
 
-.side-menu {
+export const SideMenu = styled.div`
   display: flex;
   flex-direction: column;
-}
+`;
 
-.side-wrapper {
+export const SideWrapper = styled.div`
   border-bottom: 1px solid var(--border-color);
   padding: 2.25rem 0;
   width: 9.0625rem;
-}
+`;
 
-.wrapper {
+export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
-}
+`;
 
-.header {
+export const Header = styled.div`
   display: flex;
   align-items: center;
   flex-shrink: 0;
   padding: 1.875rem;
-}
+`;
 
-.main-container {
+export const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   padding: 3.125rem 4.375rem;
   flex-grow: 1;
   overflow: auto;
   background-color: #f6f8fb;
-}
+`;
 
-.App-logo {
+export const AppLogo = styled.img`
   width: 100px;
   height: 100px;
   overflow: hidden;
   margin-top: 50px;
-} */
+`;
