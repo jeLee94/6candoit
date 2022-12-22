29c331266fe9e2f29e0fd72e765b9c1bb8e23b2f
dada992 <olive_js_933@naver.com>
2022-12-22 12:16:30 +0900

    [design] 사이드바 및 전체적인 틀

---
+++ src/pages/Mainpage/Main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Sidebar from "../../components/Layout/Sidebar/Sidebar";
+
+export default function Main() {
+  return <Sidebar />;
+}
--- src/shared/router.js
+++ src/shared/router.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
 // import Header from '../components/Layout/Header';
-import PostsContainer from '../pages/PostListPage/PostsContainer';
-import PostAddForm from '../pages/PostAddPage/PostAddForm';
+import Main from "../pages/Mainpage/Main";
+import PostAddForm from "../pages/PostAddPage/PostAddForm";
 // import PostsContainer from './pages/PostListPage/PostsContainer';
 // import PostAddForm from '../pages/PostAddPage/PostAddForm';
 // import Postpage from './pages/PostPage/Postpage';
 // import PostEditForm from './pages/PostEditPage/PostEditForm';
 
 const Wrap = styled.div`
-  width: 70%;
-  min-width: 500px;
-  margin: 100px auto;
+  max-width: 96.25rem;
+  max-height: 56.25rem;
+  height: 95vh;
   display: flex;
+  overflow: hidden;
+  border-radius: 20px;
+  width: 96rem;
+  font-size: 16px;
+  font-weight: 500;
+  box-shadow: 0 20px 50px rgba(0, 0, 0, 0.3);
+  position: relative;
 `;
 
 const Router = () => {
@@ -22,8 +29,8 @@ const Router = () => {
       {/* <Header></Header> */}
       <Wrap>
         <Routes>
-          <Route path='/' element={<PostsContainer />} />
-          <Route path='/addform' element={<PostAddForm />} />
+          <Route path="/" element={<Main />} />
+          <Route path="/addform" element={<PostAddForm />} />
           {/* <Route path='/editform' element={<PostEditForm />} /> */}
           {/* <Route path='/:id' element={<Postpage />} /> */}
         </Routes>
